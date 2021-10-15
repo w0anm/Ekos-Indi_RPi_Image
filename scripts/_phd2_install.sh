33293f5ac1651f5c015736deed69566c22aa7804
w0anm <w0anm73@gmail.com>
2021-10-14 20:50:04 -0500

    updated phd2 script

--- scripts/_phd2_install.sh
+++ scripts/_phd2_install.sh
@@ -1,6 +1,45 @@
 #!/bin/bash
 
 # run as eko user
+user=ekos
+if [ ! "$(whoami)" = "$user" ]; then
+   echo "This script must be run as $user" 
+   exit 1
+fi
+
+echo
+echo "This scripts builds the phd2 software from source.  For more"
+echo "information, visit:"
+echo "     https://github.com/OpenPHDGuiding/phd2"
+echo
+echo "You can build the PHD2 Guiding software now, or later."
+
+while true 
+do
+    echo
+    echo -n "Do you wish to build PHD2 Guiding software now [y,n]: " ; read answer
+
+    case $answer in
+
+      n)  ## do it later
+            mv ~/_phd2_install.sh ~/bin/_phd2_install.sh
+            echo
+            echo "You can re-execute this script if you would like to install PHD2 at a later time."
+            echo "Just enter _phd2_install.sh and the command line prompt."
+            echo
+            exit 0
+            ;;
+      y) ## do it now
+           echo
+           echo "Starting PHD2 build please wait until completed..."
+           break
+           ;;
+
+      *)   echo "Error: enter 'y' or 'n' "
+           ;;
+     esac
+done
+
 # phd2 make and install
 
 # setup build env
@@ -22,4 +61,8 @@ sudo make install
 # move file away so that it will not re-build
 mv ~/_phd2_install.sh ~/bin/.
 
+echo
+echo "Build and installation is completed..."
+echo
 
+exit
