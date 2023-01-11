f689fc4583c380ddc2ce95b57796b407b3a243db
w0anm <w0anm73@gmail.com>
2023-01-11 15:17:58 -0600

    Fixed and released v9.1

--- make_dist.sh
+++ make_dist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIST=Ekos_Indi_Rpi4_v9
+DIST=Ekos_Indi_Rpi4_v9.1
 
 if [ ! -d ./dist/$DIST ] ; then
     mkdir -p ./dist/$DIST
@@ -15,7 +15,7 @@ echo "Copying files to distribution directory..."
 mv scripts.tar ./dist/$DIST/.
 cp scripts/run_first.sh ./dist/$DIST/.
 cp scripts.tar ./dist/$DIST/.
-cp Ekos_Indi_SD_Install.pdf ./dist/$DIST/.
+cp Ekos_Indi_SD_Install_Ubuntu_22.04.pdf ./dist/$DIST/.
 
 echo
 echo "Creating Distribution zip file..."
